57d84db8a57761e59939a9cbecdc65c68e580d67
rdoering <rdoering.info@gmail.com>
2019-05-26 21:36:51 +0200

    init

---
+++ client/main.php
@@ -0,0 +1,15 @@
+<?php
+
+use Prometheus\CollectorRegistry;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$adapter = new Prometheus\Storage\InMemory();
+$registry = new CollectorRegistry($adapter);
+$registry->register
+$counter = $registry->registerCounter('test', 'some_counter', 'it increases', ['type', 'color']);
+$count = rand(0, 10);
+echo $count;
+$counter->incBy($count, ['blue', 'red']);
+$pushGateway = new \Prometheus\PushGateway('localhost:9091');
+$pushGateway->push($registry, 'my_job', ['instance'=>'foo']);
